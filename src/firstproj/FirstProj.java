f7276fd796c479f4ed51a17d6b5a0457b27d09d4
kooki <kooki@DESKTOP-MUBQ6AS>
2024-09-11 08:27:33 +0800

    FirstProj

---
+++ src/firstproj/FirstProj.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package firstproj;
+
+/**
+ *
+ * @author kooki
+ */
+public class FirstProj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
